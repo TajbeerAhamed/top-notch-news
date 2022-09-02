ad3f1e1559c76647eab770b64ef4a0ab3d2a6cdb
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-09-02 22:32:50 +0600

    navbar added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('connecteed')
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.data)
+}
+
+const displayNews = news => {
+
+}
+
+// loadNews()
